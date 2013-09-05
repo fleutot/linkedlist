3c6cd7f5224c8d84f82be2d065a00cc5292f64e1
Gauthier Östervall <gauthier@ostervall.se>
2013-09-05 21:24:49 +0200

    New helper functions in test, smaller and better test cases.

--- test/linkedlist_test.c
+++ test/linkedlist_test.c
@@ -15,7 +15,7 @@ Copyright (c) 2013 Gauthier Fleutot Ostervall
 // Module macros
 //******************************************************************************
 // Number of elements in array x.
-#define NUMBER_OF_ELEMENTS(x) (sizeof (x) / sizeof (x[0]))
+#define NB_ELEMENTS(x) (sizeof (x) / sizeof (x[0]))
 
 
 //******************************************************************************
@@ -25,7 +25,6 @@ Copyright (c) 2013 Gauthier Fleutot Ostervall
 //******************************************************************************
 // Module variables
 //******************************************************************************
-static int cumulative_sum;
 static int read_array[20];
 static unsigned int read_array_current_index;
 
@@ -34,15 +33,15 @@ static unsigned int read_array_current_index;
 // Function prototypes
 //******************************************************************************
 // Helper functions.
-static void cumulative_add_reset(void);
-static void cumulative_add(void const * const a);
 static void display(void const * const a);
 static void list_populate(linkedlist_t * const list,
                           const int data[],
                           const unsigned int number_of_elements);
 static void list_read_to_array_reset(void);
 static void list_read_to_array(void const * const a);
-
+static bool int_arrays_equal(int const * const a,
+                             int const * const b,
+                             const unsigned int size);
 
 // Test functions.
 static void test_linkedlist_init(void);
@@ -81,58 +80,36 @@ static void test_linkedlist_init(void)
 }
 
 
-// Helper function to be used as a callback. Its prototype much match that of
-// the callback parameter of linkedlist_run_for_all(), which explains the use of
-// a static variable of module scope.
-static void cumulative_add(void const * const a)
-{
-    cumulative_sum += *(int *) a;
-}
-
-// Helper function.
-static void cumulative_add_reset(void)
-{
-    cumulative_sum = 0;
-}
-
 static void test_linkedlist_run_for_all(void)
 {
-    // Create a list and add all its elements together.
-    linkedlist_t *list_p = linkedlist_create();
     int data[] = {1, 2, 3, 4, 5};
-    int data_sum = 0;
 
-    for (int index = 0; index < NUMBER_OF_ELEMENTS(data); index++) {
-        linkedlist_add(list_p, &data[index]);
-        data_sum += data[index];    // For later comparison.
-    }
-    cumulative_add_reset();
-    linkedlist_run_for_all(list_p, cumulative_add);
+    linkedlist_t *list = linkedlist_create();
+    list_populate(list, data, NB_ELEMENTS(data));
 
-    assert(cumulative_sum == data_sum);
+    list_read_to_array_reset();
+    linkedlist_run_for_all(list, list_read_to_array);
 
-    linkedlist_destroy(list_p);
+    assert(int_arrays_equal(data, read_array, NB_ELEMENTS(data)));
+
+    linkedlist_destroy(list);
 }
 
 
 static void test_linkedlist_copy(void)
 {
     int data[] = {1, 2, 3, 4, 5};
-    int data_sum = 0;
 
     linkedlist_t *src = linkedlist_create();
-    for (int index = 0; index < NUMBER_OF_ELEMENTS(data); index++) {
-        linkedlist_add(src, &data[index]);
-        data_sum += data[index];    // For later comparison.
-    }
+    list_populate(src, data, NB_ELEMENTS(data));
 
     linkedlist_t *dst = linkedlist_create();
     linkedlist_copy(dst, src, sizeof data[0]);
 
-    cumulative_add_reset();
-    linkedlist_run_for_all(dst, cumulative_add);
+    list_read_to_array_reset();
+    linkedlist_run_for_all(dst, list_read_to_array);
 
-    assert(cumulative_sum == data_sum);
+    assert(int_arrays_equal(data, read_array, NB_ELEMENTS(data)));
 
     linkedlist_destroy(src);
     linkedlist_destroy(dst);
@@ -143,22 +120,18 @@ static void test_linkedlist_sublist_copy(void)
 {
     const unsigned int node_index = 2;
     int data[] = {1, 2, 3, 4, 5};
-    int data_sum = 0;
+    int result[] = {3, 4, 5};
 
     linkedlist_t *list = linkedlist_create();
-    list_populate(list, data, NUMBER_OF_ELEMENTS(data));
-
-    for (int index = node_index; index < NUMBER_OF_ELEMENTS(data); index++) {
-        data_sum += data[index];
-    }
+    list_populate(list, data, NB_ELEMENTS(data));
 
     linkedlist_t *sublist = linkedlist_create();
     linkedlist_sublist_copy(sublist, list, node_index, sizeof data[0]);
 
-    cumulative_add_reset();
-    linkedlist_run_for_all(sublist, cumulative_add);
+    list_read_to_array_reset();
+    linkedlist_run_for_all(sublist, list_read_to_array);
 
-    assert(cumulative_sum == data_sum);
+    assert(int_arrays_equal(result, read_array, NB_ELEMENTS(result)));
 
     linkedlist_destroy(list);
     linkedlist_destroy(sublist);
@@ -171,45 +144,25 @@ static void test_linkedlist_cross(void)
     const int data_b[] = {11, 12, 13, 14, 15, 16, 17};
     const unsigned int pos_a = 4;
     const unsigned int pos_b = 2;
-    // After crossing the lists at the positions above, the results must be:
-    // data_a[] == {1, 2, 3, 4, 13, 14, 15, 16, 17}
-    // data_b[] == {11, 12, 5}
-
-    int data_sum_a = 0;
-    int data_sum_b = 0;
-
-    // Generate the test sums.
-    unsigned int index;
-    for (index = 0; index < pos_a; index++) {
-        data_sum_a += data_a[index];
-    }
-    for (index = pos_b; index < NUMBER_OF_ELEMENTS(data_b); index++) {
-        data_sum_a += data_b[index];
-    }
-
-    for (index = 0; index < pos_b; index++) {
-        data_sum_b += data_b[index];
-    }
-    for (index = pos_a ; index < NUMBER_OF_ELEMENTS(data_a); index++) {
-        data_sum_b += data_a[index];
-    }
+    const int result_a[] = {1, 2, 3, 4, 13, 14, 15, 16, 17};
+    const int result_b[] = {11, 12, 5};
 
     // Create the lists, cross them and test.
     linkedlist_t *list_a = linkedlist_create();
     linkedlist_t *list_b = linkedlist_create();
 
-    list_populate(list_a, data_a, NUMBER_OF_ELEMENTS(data_a));
-    list_populate(list_b, data_b, NUMBER_OF_ELEMENTS(data_b));
+    list_populate(list_a, data_a, NB_ELEMENTS(data_a));
+    list_populate(list_b, data_b, NB_ELEMENTS(data_b));
 
     linkedlist_cross(list_a, pos_a, list_b, pos_b);
 
-    cumulative_add_reset();
-    linkedlist_run_for_all(list_a, cumulative_add);
-    assert(cumulative_sum == data_sum_a);
+    list_read_to_array_reset();
+    linkedlist_run_for_all(list_a, list_read_to_array);
+    assert(int_arrays_equal(result_a, read_array, NB_ELEMENTS(result_a)));
 
-    cumulative_add_reset();
-    linkedlist_run_for_all(list_b, cumulative_add);
-    assert(cumulative_sum == data_sum_b);
+    list_read_to_array_reset();
+    linkedlist_run_for_all(list_b, list_read_to_array);
+    assert(int_arrays_equal(result_b, read_array, NB_ELEMENTS(result_b)));
 
     linkedlist_destroy(list_a);
     linkedlist_destroy(list_b);
@@ -229,50 +182,50 @@ static void test_linkedlist_cross_at_0(void)
     linkedlist_t *list_a = linkedlist_create();
     linkedlist_t *list_b = linkedlist_create();
 
-    list_populate(list_a, data_a, NUMBER_OF_ELEMENTS(data_a));
-    list_populate(list_b, data_b, NUMBER_OF_ELEMENTS(data_b));
+    list_populate(list_a, data_a, NB_ELEMENTS(data_a));
+    list_populate(list_b, data_b, NB_ELEMENTS(data_b));
 
     linkedlist_cross(list_a, pos_a, list_b, pos_b);
 
     list_read_to_array_reset();
     linkedlist_run_for_all(list_a, list_read_to_array);
-
-    for (unsigned int i = 0; i < NUMBER_OF_ELEMENTS(result_a); i++) {
-        assert(read_array[i] == result_a[i]);
-    }
+    assert(int_arrays_equal(result_a, read_array, NB_ELEMENTS(result_a)));
 
     list_read_to_array_reset();
     linkedlist_run_for_all(list_b, list_read_to_array);
-
-    for (unsigned int i = 0; i < NUMBER_OF_ELEMENTS(result_b); i++) {
-        assert(read_array[i] == result_b[i]);
-    }
+    assert(int_arrays_equal(result_b, read_array, NB_ELEMENTS(result_b)));
 
     linkedlist_destroy(list_a);
     linkedlist_destroy(list_b);
 }
 
+//------------------------------------------------------------------------------
+// Helper functions
+//------------------------------------------------------------------------------
 static void list_read_to_array_reset(void)
 {
-    for (unsigned int i = 0; i < NUMBER_OF_ELEMENTS(read_array); i++) {
+    for (unsigned int i = 0; i < NB_ELEMENTS(read_array); i++) {
         read_array[i] = 0;
     }
     read_array_current_index = 0;
 }
 
-
+// This function is meant to be used as a parameter of linkedlist_run_for_all,
+// hence the parameter format.
 static void list_read_to_array(void const * const a)
 {
     read_array[read_array_current_index] = * (int *) a;
     read_array_current_index++;
 
-    if (read_array_current_index >= NUMBER_OF_ELEMENTS(read_array)) {
+    if (read_array_current_index >= NB_ELEMENTS(read_array)) {
         fprintf(stderr, "list_read_to_array: index out of range.\n");
         read_array_current_index = 0;
     }
 }
 
 
+// This function is meant to be used as a parameter of linkedlist_run_for_all,
+// hence the parameter format.
 static void display(void const * const a)
 {
     printf("d: %d\n", *(int *) a);
@@ -286,4 +239,16 @@ static void list_populate(linkedlist_t * const list,
     for (unsigned int index = 0; index < number_of_elements; index++) {
         linkedlist_add(list, &data[index]);
     }
+}
+
+static bool int_arrays_equal(int const * const a,
+                             int const * const b,
+                             const unsigned int size)
+{
+    for (unsigned int i = 0; i < size; i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
